b6fb8027a36300e59a3c69deff0bcb89b0591b6d
yanglong <2723659854@qq.com>
2024-04-24 14:43:45 +0800

    修改命令

--- src/copy.php
+++ src/copy.php
@@ -1,4 +1,4 @@
 <?php
-echo "i am running\r\n";
+echo "I am running\r\n";
 echo __DIR__;
-//file_put_contents('../../../one.php',"<?php echo 'i am a test demo';?>");
+file_put_contents('../../../one.php',"<?php echo 'i am a test demo';?>");
