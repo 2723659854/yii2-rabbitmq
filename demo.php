c83e8d4436e4c31ff931c01357ea11b8516bf6cd
yanglong <2723659854@qq.com>
2024-04-24 15:01:33 +0800

    测试

--- demo.php
+++ demo.php
@@ -1,20 +1,21 @@
 <?php
-namespace root\yanglong;
+//namespace root\yanglong;
+//
+//require_once __DIR__.'/vendor/autoload.php';
+//
+//class Queued1 extends \Xiaosongshu\Rabbitmq\Client{
+//
+//
+//    public static function handle(array $params): int
+//    {
+//        var_dump($params);
+//        return self::ACK;
+//    }
+//}
 
-require_once __DIR__.'/vendor/autoload.php';
-
-class Queued1 extends \Xiaosongshu\Rabbitmq\Client{
-
-
-    public static function handle(array $params): int
-    {
-        var_dump($params);
-        return self::ACK;
-    }
-}
-
-Queued1::send(['name'=>'张三']);
-Queued1::consume();
+//Queued1::send(['name'=>'张三']);
+//Queued1::consume();
+var_dump(23423);
 
 
 
